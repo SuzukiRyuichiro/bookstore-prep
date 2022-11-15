82d11632b41e079715f3c8009c5cb39e570b36c5
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-15 13:12:58 +0100

    make some avatars

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
   def index
     # @books = Book.all
-    @books = Book.eager_load(:author).all
+    # @books = Book.eager_load(:author).all
     # @books = Book.preload(:author).all
+    @books = Book.includes(author: { avatar_attachment: :blob }).all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
+require 'open-uri'
+
 if Rails.env.development?
   Book.destroy_all
   Author.destroy_all
 end
 
 100.times do
-  Author.create name: Faker::Book.author
+  author = Author.create name: Faker::Book.author
+  file = URI.open('https://thispersondoesnotexist.com/image')
+  author.avatar.attach(io: file, filename: 'face.png', content_type: 'image/png')
 end
 
 1000.times do
